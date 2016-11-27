7d86e430de18d6b4e8bceb89f5ea5c1396df8308
Visman <visman2008@googlemail.com>
2016-11-27 21:37:19 +0700

    for test

---
+++ tests/ConstructTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ConstructTest extends TestCase
+{
+    public function testInit()
+    {
+        $text = "\tHello\n\tWorld!";
+        $html = '&nbsp; &nbsp; Hello<br>&nbsp; &nbsp; World!';
+        $xhtml = '&#160; &#160; Hello<br />&#160; &#160; World!';
+
+        $parser = new Parserus();
+        $this->assertEquals($html, $parser->parse($text)->getHTML());
+
+        $parser = new Parserus(ENT_HTML401);
+        $this->assertEquals($html, $parser->parse($text)->getHTML());
+
+        $parser = new Parserus(ENT_XML1);
+        $this->assertEquals($xhtml, $parser->parse($text)->getHTML());
+
+        $parser = new Parserus(ENT_XHTML);
+        $this->assertEquals($xhtml, $parser->parse($text)->getHTML());
+
+        $parser = new Parserus(ENT_HTML5);
+        $this->assertEquals($html, $parser->parse($text)->getHTML());
+
+        $parser = new Parserus(224);
+        $this->assertEquals($html, $parser->parse($text)->getHTML());
+    }
+}
