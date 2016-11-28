82a73c101f4076e07834c4b06ccc643065b81889
Visman <visman2008@googlemail.com>
2016-11-28 16:27:36 +0700

    fix 14 for test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,17 @@
 <?php
 
 #include 'Parserus.php';
+
+spl_autoload_register(
+	function($className)
+	{
+		if ($className == 'Parserus')
+		{
+			$path = __DIR__ . '/../Parserus.php';
+			if (file_exists($path))
+			{
+				include $path;
+			}
+		}
+	}
+);
